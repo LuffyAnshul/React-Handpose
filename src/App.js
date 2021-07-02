ce3dc1eab87f4bc6b88ca7914c6028551bfc2dcd
luffyanshul <anshulborawake@gmail.com>
2021-07-02 15:55:52 +0530

    gesture recognition

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import * as tf from '@tensorflow/tfjs';
 import * as handpose from '@tensorflow-models/handpose';
 import Webcam from 'react-webcam'; 
+
+import * as fp from 'fingerpose';
+import victory from './victory.png';
+import thumbs_up from './thumbs_up.png';
 
 import { drawHand } from './utlities';
 import './App.css';
@@ -9,6 +13,9 @@ import './App.css';
 function App() {
 	const webCamRef = useRef(null);
 	const canvasRef = useRef(null);
+
+	const [emoji, setEmoji] = useState(null);
+	const images = {thumbs_up: thumbs_up, victory: victory}
 
 	const runHandpose = async() => {
 		const net = await handpose.load();
@@ -41,7 +48,29 @@ function App() {
 
 			// Make Detections
 			const hand = await net.estimateHands(video);
-			console.log(hand)
+			// console.log(hand)
+
+			if (hand.length > 0) {
+				const GE = new fp.GestureEstimator([
+					fp.Gestures.VictoryGesture,
+					fp.Gestures.ThumbsUpGesture
+				])
+
+				const gesture = await GE.estimate(hand[0].landmarks, 8);
+				// console.log(gesture)
+				if (gesture.gestures !== undefined && gesture.gestures.length > 0) {
+					const confidence = gesture.gestures.map(
+						(prediction) => prediction.confidence
+					);
+
+					const maxConfidence = confidence.indexOf(
+						Math.max.apply(null, confidence)
+					);
+
+					setEmoji(gesture.gestures[maxConfidence].name);
+					console.log(emoji)
+				}
+			}
 
 			// Draw mesh
 			const ctx = canvasRef.current.getContext("2d");
@@ -62,6 +91,16 @@ function App() {
 					ref={canvasRef}
 					style={{ position: 'absolute', marginLeft: "auto", marginRight: "auto", left: 0, right: 0, textAlign: 'center', zIndex: 9, width: 640, height: 480 }}
 				/>
+
+				{
+					emoji !== null 
+					? 
+					<img 
+						src={images[emoji]} 
+						style={{ position: 'absolute', marginLeft: "auto", marginRight: "auto", left: 950, top: 50, textAlign: 'center', height:100, zIndex: 10 }} 
+					/>
+					: ""
+				}
 			</header>
 		</div>
 	);
